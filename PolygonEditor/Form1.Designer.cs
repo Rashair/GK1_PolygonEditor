26ce9b1595f18ee731dad2fed8862ab194bf6a0d
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2019-10-03 23:58:00 +0200

    Add project files.

---
+++ PolygonEditor/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace GraphEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.EditionGroupBox = new System.Windows.Forms.GroupBox();
+            this.ClearGraphButton = new System.Windows.Forms.Button();
+            this.RemoveVertexButton = new System.Windows.Forms.Button();
+            this.ColorBox = new System.Windows.Forms.PictureBox();
+            this.ColorButton = new System.Windows.Forms.Button();
+            this.bitMap = new System.Windows.Forms.PictureBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.EditionGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ColorBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bitMap)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.bitMap, 0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            // 
+            // tableLayoutPanel2
+            // 
+            resources.ApplyResources(this.tableLayoutPanel2, "tableLayoutPanel2");
+            this.tableLayoutPanel2.Controls.Add(this.EditionGroupBox, 0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            // 
+            // EditionGroupBox
+            // 
+            resources.ApplyResources(this.EditionGroupBox, "EditionGroupBox");
+            this.EditionGroupBox.Controls.Add(this.ClearGraphButton);
+            this.EditionGroupBox.Controls.Add(this.RemoveVertexButton);
+            this.EditionGroupBox.Controls.Add(this.ColorBox);
+            this.EditionGroupBox.Controls.Add(this.ColorButton);
+            this.EditionGroupBox.Name = "EditionGroupBox";
+            this.EditionGroupBox.TabStop = false;
+            // 
+            // ClearGraphButton
+            // 
+            resources.ApplyResources(this.ClearGraphButton, "ClearGraphButton");
+            this.ClearGraphButton.Name = "ClearGraphButton";
+            this.ClearGraphButton.UseVisualStyleBackColor = true;
+            this.ClearGraphButton.Click += new System.EventHandler(this.ClearGraphButton_Click);
+            // 
+            // RemoveVertexButton
+            // 
+            resources.ApplyResources(this.RemoveVertexButton, "RemoveVertexButton");
+            this.RemoveVertexButton.Name = "RemoveVertexButton";
+            this.RemoveVertexButton.UseVisualStyleBackColor = true;
+            this.RemoveVertexButton.Click += new System.EventHandler(this.RemoveVertexButton_Click);
+            // 
+            // ColorBox
+            // 
+            resources.ApplyResources(this.ColorBox, "ColorBox");
+            this.ColorBox.Name = "ColorBox";
+            this.ColorBox.TabStop = false;
+            this.ColorBox.Paint += new System.Windows.Forms.PaintEventHandler(this.ColorBox_Paint);
+            // 
+            // ColorButton
+            // 
+            resources.ApplyResources(this.ColorButton, "ColorButton");
+            this.ColorButton.Name = "ColorButton";
+            this.ColorButton.UseVisualStyleBackColor = true;
+            this.ColorButton.Click += new System.EventHandler(this.ColorButton_Click);
+            // 
+            // bitMap
+            // 
+            resources.ApplyResources(this.bitMap, "bitMap");
+            this.bitMap.BackColor = System.Drawing.Color.White;
+            this.bitMap.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.bitMap.Name = "bitMap";
+            this.bitMap.TabStop = false;
+            this.bitMap.Paint += new System.Windows.Forms.PaintEventHandler(this.BitMap_Paint);
+            this.bitMap.MouseClick += new System.Windows.Forms.MouseEventHandler(this.BitMap_MouseClick);
+            this.bitMap.MouseDown += new System.Windows.Forms.MouseEventHandler(this.BitMap_MouseDown);
+            this.bitMap.MouseMove += new System.Windows.Forms.MouseEventHandler(this.BitMap_MouseMove);
+            this.bitMap.MouseUp += new System.Windows.Forms.MouseEventHandler(this.BitMap_MouseUp);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.EditionGroupBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ColorBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bitMap)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.GroupBox EditionGroupBox;
+        private System.Windows.Forms.PictureBox bitMap;
+        private System.Windows.Forms.Button ColorButton;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.PictureBox ColorBox;
+        private System.Windows.Forms.Button RemoveVertexButton;
+        private System.Windows.Forms.Button ClearGraphButton;
+    }
+}
+
---
+++ PolygonEditor/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Resources;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace GraphEditor
+{
+    public partial class Form1 : Form
+    {
+        private const int vertexRadius = 15;
+        private static readonly StringFormat format = new StringFormat()
+        {
+            LineAlignment = StringAlignment.Center,
+            Alignment = StringAlignment.Center
+        };
+        private static readonly Size locationAdjustment = new Size(-vertexRadius / 2, -vertexRadius / 2);
+        private static readonly Pen pen = new Pen(Color.Black, 2);
+        private static readonly Pen edgePen = new Pen(Color.Black, 2);
+        private readonly ComponentResourceManager resources;
+
+        private enum Action { None, Move }
+        private Action current;
+
+        private Point lastPosition;
+        private Size previousLocation;
+        private Vertex selectedVertex;
+        private int selectedVertexIndex;
+        private Rectangle rectangle;
+
+        private Bitmap canvas;
+
+        private class Vertex
+        {
+            public Point point;
+
+            public Vertex(Point point)
+            {
+                this.point = point;
+            }
+
+            public override bool Equals(object obj)
+            {
+                return obj is Vertex vertex &&
+                       point.Equals(vertex.point);
+            }
+
+            public override int GetHashCode()
+            {
+                var hashCode = -1667306863;
+                hashCode = hashCode * -1521134295 + point.GetHashCode();
+                return hashCode;
+            }
+        };
+        private List<Vertex> Vertices;
+        private List<HashSet<Vertex>> Edges;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            canvas = new Bitmap(bitMap.Size.Width, bitMap.Size.Height);
+            rectangle = new Rectangle() { Size = new Size(vertexRadius, vertexRadius) };
+            Vertices = new List<Vertex>();
+            Edges = new List<HashSet<Vertex>>();
+            resources = new ComponentResourceManager(typeof(Form1));
+        }
+
+        private void BitMap_Paint(object sender, PaintEventArgs e)
+        {
+            // Bitmap setpixel
+            using (Graphics canvasGraphics = Graphics.FromImage(canvas))
+            {
+                canvasGraphics.Clear(Color.White);
+                for (int i = 0; i < Vertices.Count; ++i)
+                {
+                    foreach (Vertex vertex in Edges[i])
+                    {
+                        canvasGraphics.DrawLine(edgePen, Vertices[i].point, vertex.point);
+                    }
+                }
+
+                for (int i = 0; i < Vertices.Count; ++i)
+                {
+                    rectangle.Location = Vertices[i].point + locationAdjustment;
+                    canvasGraphics.FillEllipse(Brushes.White, rectangle);
+                    if (Vertices[i] == selectedVertex)
+                    {
+                        var prevColor = pen.Color;
+                        pen.Color = Color.Red;
+                        pen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+                        canvasGraphics.FillEllipse(pen.Brush, rectangle);
+                        pen.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+                        pen.Color = prevColor;
+                    }
+                    else
+                    {
+                        canvasGraphics.FillEllipse(pen.Brush, rectangle);
+                    }
+                }
+            }
+            e.Graphics.DrawImage(canvas, 0, 0);
+
+            pen.Color = colorDialog1.Color;
+
+            RemoveVertexButton.Enabled = selectedVertex != null;
+        }
+
+
+        private void BitMap_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                lastPosition = new Point(e.X, e.Y);
+                (Vertex vertex, int ind) = ClosestOrDefault();
+                if (vertex == null)
+                {
+                    Vertices.Add(new Vertex(lastPosition));
+                    Edges.Add(new HashSet<Vertex>());
+                    bitMap.Invalidate();
+                }
+                else if (selectedVertex != null && vertex != selectedVertex)
+                {
+                    if (Edges[selectedVertexIndex].Contains(vertex))
+                    {
+                        Edges[selectedVertexIndex].Remove(vertex);
+                        Edges[ind].Remove(selectedVertex);
+                    }
+                    else
+                    {
+                        Edges[selectedVertexIndex].Add(vertex);
+                        Edges[ind].Add(selectedVertex);
+                    }
+                    bitMap.Invalidate();
+                }
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                lastPosition = new Point(e.X, e.Y);
+                if (selectedVertex != null)
+                {
+                    bitMap.Invalidate();
+                }
+
+                (selectedVertex, selectedVertexIndex) = ClosestOrDefault();
+                if (selectedVertex != null)
+                {
+                    bitMap.Invalidate();
+                }
+            }
+        }
+
+        private void BitMap_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.XButton1)
+            {
+                current = Action.Move;
+                previousLocation = (Size)e.Location;
+            }
+        }
+
+        private void BitMap_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.XButton1)
+            {
+                current = Action.None;
+                if (selectedVertex != null)
+                {
+                    var area = bitMap.ClientRectangle;
+                    if (selectedVertex.point.Y < area.Y)
+                        selectedVertex.point.Y = area.Y;
+                    else if (selectedVertex.point.Y > area.Y + area.Height)
+                        selectedVertex.point.Y = area.Y + area.Height;
+
+                    if (selectedVertex.point.X < area.X)
+                        selectedVertex.point.X = area.X;
+                    else if (selectedVertex.point.X > area.X + area.Width)
+                        selectedVertex.point.X = area.X + area.Width;
+
+                    bitMap.Invalidate();
+                }
+            }
+        }
+
+        private void BitMap_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (current == Action.Move && selectedVertex != null)
+            {
+                selectedVertex.point += (Size)e.Location - previousLocation;
+                previousLocation = (Size)e.Location;
+                bitMap.Invalidate();
+            }
+        }
+
+        private (Vertex, int) ClosestOrDefault()
+        {
+            Vertex result = null;
+            int index = -1;
+            double minDist = double.PositiveInfinity;
+            for (int i = 0; i < Vertices.Count; ++i)
+            {
+                if (VerticesIntersect(Vertices[i].point, lastPosition) || Vertices[i].point.Equals(lastPosition))
+                {
+                    double currDist = Distance(Vertices[i].point, lastPosition);
+                    if (minDist > currDist)
+                    {
+                        minDist = currDist;
+                        result = Vertices[i];
+                        index = i;
+                    }
+                }
+            }
+
+            return (result, index);
+        }
+
+        private bool VerticesIntersect(Point p1, Point p2)
+        {
+            return (p1.X - p2.X) * (p1.X - p2.X) + (p1.Y - p2.Y) * (p1.Y - p2.Y)
+                <= vertexRadius * vertexRadius;
+        }
+
+        private double Distance(Point p1, Point p2)
+        {
+            return (p1.X - p2.X) * (p1.X - p2.X) + (p1.Y - p2.Y) * (p1.Y - p2.Y);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete && selectedVertex != null)
+            {
+                RemoveVertex();
+            }
+        }
+
+        // Right section
+
+        private void ColorButton_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pen.Color = colorDialog1.Color;
+                ColorBox.Invalidate();
+
+                if (selectedVertex != null)
+                {
+                    bitMap.Invalidate();
+                }
+            }
+        }
+
+        private void ColorBox_Paint(object sender, PaintEventArgs e)
+        {
+            ColorBox.BackColor = pen.Color;
+        }
+
+        private void RemoveVertexButton_Click(object sender, EventArgs e)
+        {
+            RemoveVertex();
+        }
+
+        private void RemoveVertex()
+        {
+            Vertices.RemoveAt(selectedVertexIndex);
+            for (int i = 0; i < Edges.Count; ++i)
+            {
+                Edges[i].Remove(selectedVertex);
+
+            }
+            Edges.RemoveAt(selectedVertexIndex);
+
+            selectedVertex = null;
+            current = Action.None;
+            RemoveVertexButton.Enabled = false;
+            bitMap.Invalidate();
+        }
+
+        private void ClearGraphButton_Click(object sender, EventArgs e)
+        {
+            ClearGraph();
+            bitMap.Invalidate();
+        }
+
+        private void ClearGraph()
+        {
+            Vertices.Clear();
+            Edges.Clear();
+            current = Action.None;
+            selectedVertex = null;
+            RemoveVertexButton.Enabled = false;
+        }
+    }
+}
